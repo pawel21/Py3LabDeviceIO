bc56c62b5a022ec36d6d5f8b37fd14babcd90950
Paweł Gliwny <p.gliwny@gmail.com>
2016-12-13 18:56:02 +0100

    fixed code to python3

--- Py3LabDeviceIO/utils/IODevice.py
+++ Py3LabDeviceIO/utils/IODevice.py
@@ -3,12 +3,12 @@ import signal
 
 
 class IODevice:
-    def __init__(self, device):
-        self.device = device
-        self.file_descriptor = os.open(device, os.O_RDWR)
+    def __init__(self, path_to_device):
+        self.device = path_to_device
+        self.file_descriptor = os.open(path_to_device, os.O_RDWR | os.O_NOCTTY)
 
     def write(self, command):
-        os.write(self.file_descriptor, command)
+        os.write(self.file_descriptor, command.encode())
 
     def read(self, length=4000):
         return os.read(self.file_descriptor, length)
@@ -30,3 +30,6 @@ class IODevice:
 
     def handler(self, signum, frame):
         pass
+
+    def __del__(self):
+        self.close()
